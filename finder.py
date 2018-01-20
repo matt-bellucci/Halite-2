70c008d823e54640b57c809040242c75d89e2abc
shamrodia74 <matt7469@gmail.com>
2018-01-20 19:23:58 +0100

    custom_bot
    le bot que matt a fait

--- finder.py
+++ finder.py
@@ -8,22 +8,14 @@ def find_closest_planet(ship,game_map,is_owned=False, isFriendly=False):
 	entities_by_distance = game_map.nearby_entities_by_distance(ship)
 	nearest_planet = None
 	for distance in sorted(entities_by_distance):
-                nearest_planet = next((nearest_entity for nearest_entity in entities_by_distance[distance] if (isinstance(nearest_entity, hlt.entity.Planet) and (nearest_entity.is_owned == is_owned) and ((nearest_entity.owner==me)==isFriendly))),None)
-    if (nearest_planet==None):
-    	logging.info("Planete non trouvee")
-   	else:
-   		logging.info("Planete trouvee")
-    return nearest_planet
+		nearest_planet = next((nearest_entity for nearest_entity in entities_by_distance[distance] if (isinstance(nearest_entity, hlt.entity.Planet) and (nearest_entity.is_owned() == is_owned) and ((nearest_entity.owner==me)==isFriendly))),None)
+		if nearest_planet:
+			break
+	if nearest_planet==None:
+		logging.info("Planete non trouvee")
+		logging.info(nearest_planet.id)
+	else:
+		logging.info("Planete trouvee")
+	return nearest_planet
 
-def find_closest_ship(ship,game_map,isFriendly=False):
-	# retourne le vaisseau allie ou ennemi le plus proche de ship
-	me = game_map.get_me()
-	entities_by_distance = game_map.nearby_entities_by_distance(ship)
-	nearest_ship = None
-	for distance in sorted(entities_by_distance):
-                nearest_ship = next((nearest_entity for nearest_entity in entities_by_distance[distance] if (isinstance(nearest_entity, hlt.entity.Ship) and ((nearest_entity.owner==me)==isFriendly))),None)
-    if (nearest_ship==None):
-    	logging.info("Vaisseau non trouve")
-    else:
-    	logging.info("Vaisseau trouve")
-    return nearest_ship	
+
