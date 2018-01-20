fb249aad7c2dee401fa960173688cbea505ee5c6
shamrodia74 <matt7469@gmail.com>
2018-01-20 19:35:25 +0100

    ajout de closest ship

--- utils.py
+++ utils.py
@@ -19,3 +19,19 @@ def find_closest_planet(ship,game_map,is_owned=False, isFriendly=False):
 	return nearest_planet
 
 
+def find_closest_ship(ship,game_map, isFriendly=False):
+	# par défaut, renvoie la planete non occupee la plus proche, sinon renvoie la planete la plus proche en fonction de son proprietaire
+	me = game_map.get_me()
+	entities_by_distance = game_map.nearby_entities_by_distance(ship)
+	nearest_ship = None
+	for distance in sorted(entities_by_distance):
+		nearest_ship = next((nearest_entity for nearest_entity in entities_by_distance[distance] if (isinstance(nearest_entity, hlt.entity.Ship) and ((nearest_entity.owner==me)==isFriendly))),None)
+		if nearest_planet:
+			break
+	if nearest_ship==None:
+		logging.info("Planete non trouvee")
+		logging.info(nearest_planet.id)
+	else:
+		logging.info("Planete trouvee")
+	return nearest_ship
+
